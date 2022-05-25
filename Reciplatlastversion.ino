82fee49f2f2d55f5239c4b4d6f0946425f4756d2
jsfuertesdev <jsfuertesdev@gmail.com>
2022-05-25 13:47:24 -0500

    adding the download botton

---
+++ Reciplatlastversion.ino
@@ -0,0 +1,48 @@
+/**********************************************************************************
+ * TITULO: Codigo Arduino Para Etch a Sketch
+ * AUTOR: Jhimmy Astoraque Durán
+ * DESCRIPCION: Este es el código del capítulo 1 de la serie Arduino-HTML5 del canal JADSA TV.
+ * CANAL YOUTUBE: https://www.youtube.com/c/jadsatv
+ * © 2019
+ * *******************************************************************************/
+
+// Variables for pot reading
+int valX;
+int valY;
+bool changedX;
+bool changedY;
+
+// for normalized values
+int horizontalVal = 0;
+int verticalVal = 0;
+
+// Some allowance to reduce noise down
+const int threshold = 5;
+
+void setup()
+{
+    Serial.begin(115200);
+}
+
+void loop()
+{
+    valX = analogRead(A0);
+    valY = analogRead(A2);
+
+    changedX = abs( valX - horizontalVal) >= threshold || ( valX == 0 && horizontalVal != 0) || (valX == 1023 && horizontalVal != 1023);
+    changedY = abs( valY - verticalVal ) >= threshold || (valY == 0 && verticalVal != 0 ) || ( valY ==1023 && verticalVal != 1023);
+
+    if (changedX || changedY){
+        int invertedX = abs(1024 - valX);
+        int invertedY = abs(1024 - valY);
+
+        // Serial com (x,y)
+        Serial.print(invertedX);
+        Serial.print(",");
+        Serial.println(invertedY);
+        horizontalVal = valX;
+        verticalVal = valY;
+    }
+
+    delay(100);
+}
